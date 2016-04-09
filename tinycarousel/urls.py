3097af9a5e7cdf3dbf156bc92c61a8da1555b6ca
Shan Pruthi <shanpruthi@msn.com>
2016-04-09 14:06:48 -0400

    Connect static files to templates

--- tinycarousel/urls.py
+++ tinycarousel/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'about', views.bout, name='about'),
-    url(r'products', views.shit, name='products'),
+    url(r'about', views.about, name='about'),
+    url(r'products', views.products, name='products'),
 
 ]
